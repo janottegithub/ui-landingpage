beaab0cf74f9b8ffcaac098323ccc2a495bb9b34
jan otte <janotte@MBP-van-jan.lan>
2019-08-08 17:12:34 +0200

    gsap navbar

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const slider = document.querySelector(".slider");
 const logo = document.querySelector("#logo");
 const hamburger = document.querySelector(".hamburger");
 const headline = document.querySelector(".headline");
-const lines = document.querySelector("#lines");
+const bgImage = document.querySelector("#bgImage");
 const li = document.querySelectorAll("li");
+var btn = document.querySelector('.toggle-btn');
+
 
 const tl = new TimelineMax();
 
@@ -34,26 +36,60 @@ tl.fromTo(
     {opacity: 1, x: 0},
     "-=0.5"
     )
-.fromTo(
+/*.fromTo(
     hamburger,
     0.5, 
     {opacity:0, x: 30}, 
     {opacity: 1, x: 0},
     "-=0.5"
     )
+*/
 .fromTo(
-        lines,
+    btn,
+    0.5, 
+    {opacity:0, x: 30}, 
+    {opacity: 1, x: 0},
+    "-=0.5"
+        )
+
+.fromTo(
+        bgImage,
         2, 
         {opacity:0}, 
         {opacity: 1},
-        );
-
-
-hamburger.addEventListener('click', function(){
-    tl.fromTo(
-        li,
-        1, 
-        {opacity:0, x: 30}, 
-        {opacity: 1, x: 0},
         )
+
+/*///////////////part for the flexible navbar////////////////*/
+
+var bgImage2 = document.querySelector('.bgImage2');
+var tl2 = new TimelineMax({paused: true});
+
+tl2.to(".one", 0.8,{
+    y:6,
+    rotation: 45,
+    ease: Expo.easeInOut
 });
+tl2.to(".two", 0.8,{
+    y:-6,
+    rotation: -45,
+    ease: Expo.easeInOut,
+    delay: -0.8
+})
+
+tl2.to(".menu", 2,{
+    top:"0%",
+    ease: Expo.easeInOut,
+    delay: -2
+})
+
+tl2.staggerFrom(".menu ul li", 2, {x: -200, opacity: 0, ease:Expo.easeOut}, 0.3)
+
+tl2.reverse();
+
+var btn = document.querySelector('.toggle-btn');
+btn.addEventListener('click', () => {
+ tl2.reversed(!tl2.reversed());
+ bgImage2.style.display = "block";
+ //bgImage.style.display = "none";
+
+})
